6d80ec860360c25cc27419be6ab03a33e4382b93
NeiVilPiz <neithan.villarroel@alu.ceacfp.com>
2026-06-02 11:27:00 +0200

    arreglo de url

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,6 @@
 import express from "express"
 import cors from "cors"
 import projectsRoutes from "./routes/projects.routes"
-import { Project } from "./types/project"
-
-const test: Project = {
-  id: 1,
-  title: "test",
-  category: "test",
-  description: "test",
-  problem: "test",
-  features: [],
-  targetUsers: [],
-  roadmap: []
-}
 
 const app = express()
 
@@ -21,6 +9,8 @@ app.use(express.json())
 
 app.use("/api/projects", projectsRoutes)
 
-app.listen(3001, () => {
-  console.log("Server running on http://localhost:3001")
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
